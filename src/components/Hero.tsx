e2aee21979fcbe92064efdacbf0afd9429a8521d
cristohub <daw202530@estudiantes.salesianasnsp.es>
2025-06-28 12:53:43 +0200

    updte link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -142,7 +142,7 @@ const Hero: React.FC = () => {
               variants={textVariants}
             >
               <a
-                href="https://linkedin.com/in/tuusuario"
+                href="www.linkedin.com/in/cristofersani"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="icon-hover text-primary d-flex align-items-center"
